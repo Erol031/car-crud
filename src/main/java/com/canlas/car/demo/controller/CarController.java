dedc90521b34ac2d25cfe59014bc578ef74cbf82
unknown 111 <mmtungol.student@ua.edu.ph>
2024-05-15 20:52:46 +0900

    first commit

---
+++ src/main/java/com/canlas/car/demo/controller/CarController.java
@@ -0,0 +1,42 @@
+package com.tungol.car.demo.controller;
+
+import com.tungol.car.demo.model.Car;
+import com.tungol.car.demo.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cars")
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+    @GetMapping("/{id}")
+    public Car getCarById(@PathVariable Long id) {
+        return carService.getCarById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Car createCar(@RequestBody Car car) {
+        return carService.saveCar(car);
+    }
+
+    @PutMapping("/{id}")
+    public Car updateCar(@PathVariable Long id, @RequestBody Car car) {
+        car.setId(id);
+        return carService.saveCar(car);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        carService.deleteCar(id);
+    }
+}
